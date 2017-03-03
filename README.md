f2e47fccc47b7ebbe65eaedde13fba7352fecbfa
americast <sayan.sinha@iitkgp.ac.in>
2017-03-04 02:40:45 +0530

    abstract

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # DataDeps
+--------------
+WORK IN PROGRESS
+--------------
+
+This project involves building a “BinDeps.jl for data” which would make the creation of data-providing packages easier. The package would make it easy to download / unzip large files and check their integrity them in a cross-platform way. Facilities for downloading specific datasets can then be built on top of this.
+
--- src/DataDeps.jl
+++ src/DataDeps.jl
@@ -1,5 +1,26 @@
 module DataDeps
+urls=Dict(
+  "CIFAR-10"  =>	"https://www.cs.toronto.edu/~kriz/cifar-10-binary.tar.gz",
+  "CIFAR-100" =>	"https://www.cs.toronto.edu/~kriz/cifar-100-binary.tar.gz",
+)
 
-# package code goes here
+function getdata(dir,key)
+    global urls
+    mkpath(dir)
+    path = download(urls[key])
+    run(unpack_cmd(path,dir,".gz",".tar"))
+end
 
+function search(text::AbstractString)
+    for i in urls
+        if (contains(i[1],text))
+            println("Found $(i[1]). Continue (Y/N)?")
+            choice=readline(STDIN)
+            if (choice[1]=='y' || choice[1]=='Y')
+                dirhere=joinpath(Pkg.dir("DataDeps"), "datasets/$(i[1])")
+      	        getdata(dirhere,i[1])
+            end
+        end
+    end
+end
 end # module
